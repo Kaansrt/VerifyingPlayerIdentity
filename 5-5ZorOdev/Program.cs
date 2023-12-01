b60a056db64e7fb5819ae57a66d979b96272c884
Murat Kaan Sert <kaans1221@gmail.com>
2023-12-02 00:29:26 +0300

    Add project files.

---
+++ 5-5ZorOdev/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _5_5ZorOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer { Id = 1, BirthYear = 2001, FirstName = "KAAN", LastName = "SERT", IdentityNumber = 12345 });
+        }
+    }
+}
+
